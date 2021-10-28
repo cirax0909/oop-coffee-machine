5a0d3ae8727b876cf021c98687f409b9e24a3417
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-10-28 15:30:08 -0400

    Oop version of coffee-machine

---
+++ main.py
@@ -0,0 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+machine_on = True
+while machine_on:
+    option = menu.get_items()
+    choice = input(f"What would you like? {option}:")
+    if choice == "off":
+        machine_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        can_make = coffee_maker.is_resource_sufficient(drink)
+        if can_make:
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
